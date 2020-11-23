914810f630a92bfec7d812914c1016a6c99d1b1b
Vincent Wang <vwang314@gmail.com>
2020-11-23 15:03:12 -0600

    motor test

--- main.cpp
+++ main.cpp
@@ -16,16 +16,30 @@ void dist4(int distance){
     printf("Sens4 Distance %d mm\r\n", distance);
 }
 
-ultrasonic sens1(p5, p6, .1, 1, &dist1);
-ultrasonic sens2(p7, p8, .1, 1, &dist2);
-ultrasonic sens3(p9, p10, .1, 1, &dist3);
-ultrasonic sens4(p11, p12, .1, 1, &dist4);
+ultrasonic sens1(p5, p6, .1, .2, &dist1);
+ultrasonic sens2(p7, p8, .1, .2, &dist2);
+ultrasonic sens3(p9, p10, .1, .2, &dist3);
+ultrasonic sens4(p11, p12, .1, .2, &dist4);
 Serial pc(USBTX, USBRX);
-Motor left(p21, p24, p25, 1);
-Motor right(p22, p29, p30, 1);
+Motor left(p21, p25, p26, 1);
+Motor right(p22, p30, p29, 1);
 Serial blue(p13, p14);
 
 int main(){
+    left.speed(0.5);
+    wait(1);
+    left.stop(0.5);
+    right.speed(0.5);
+    wait(1);
+    right.stop(0.5);
+    wait(1);
+    left.speed(-0.5);
+    wait(1);
+    left.stop(0.5);
+    right.speed(-0.5);
+    wait(1);
+    right.stop(0.5);
+
     LSM9DS1 dof(p28, p27, 0xD6, 0x3C);
     dof.begin();
     if(!dof.begin()){
