33a0deac7a5c74c5019318bc4de06e8e5ec2991f
Vincent Wang <vwang314@gmail.com>
2020-11-23 14:17:33 -0600

    i/o setup and test

--- main.cpp
+++ main.cpp
@@ -3,26 +3,36 @@
 #include "LSM9DS1.h"
 #include "motordriver.h"
 
-void dist(int distance){
-    printf("Distance %d mm\r\n", distance);
+void dist1(int distance){
+    printf("Sens1 Distance %d mm\r\n", distance);
+}
+void dist2(int distance){
+    printf("Sens2 Distance %d mm\r\n", distance);
+}
+void dist3(int distance){
+    printf("Sens3 Distance %d mm\r\n", distance);
+}
+void dist4(int distance){
+    printf("Sens4 Distance %d mm\r\n", distance);
 }
 
-ultrasonic sens1(p5, p6, .1, 1, &dist);
-ultrasonic sens2(p7, p8, .1, 1, &dist);
-ultrasonic sens3(p9, p10, .1, 1, &dist);
-ultrasonic sens4(p11, p12, .1, 1, &dist);
+ultrasonic sens1(p5, p6, .1, 1, &dist1);
+ultrasonic sens2(p7, p8, .1, 1, &dist2);
+ultrasonic sens3(p9, p10, .1, 1, &dist3);
+ultrasonic sens4(p11, p12, .1, 1, &dist4);
 Serial pc(USBTX, USBRX);
-Motor left(p21, p25, p26, 1);
-Motor right(p22, p23, p24, 1);
-Serial blue(p28, p27);
+Motor left(p21, p24, p25, 1);
+Motor right(p22, p29, p30, 1);
+Serial blue(p13, p14);
 
 int main(){
-    LSM9DS1 9dof(p13, p14, 0xD6, 0x3C);
-    9dof.begin();
-    if(!9dof.begin()){
+    LSM9DS1 dof(p28, p27, 0xD6, 0x3C);
+    dof.begin();
+    if(!dof.begin()){
         pc.printf("Failed to communicate with LSM9DS1.\n");
     }
-    9dof.calibrate();
+    dof.calibrate();
+    wait(10);
     sens1.startUpdates();
     sens2.startUpdates();
     sens3.startUpdates();
@@ -34,13 +44,13 @@ int main(){
         sens3.checkDistance();
         sens4.checkDistance();
 
-        imu.readTemp();
-        imu.readMag();
-        imu.readGyro();
+        dof.readTemp();
+        dof.readMag();
+        dof.readGyro();
 
-        pc.printf("gyro: %d %d %d\n\r", imu.gx, imu.gy, imu.gz);
-        pc.printf("accel: %d %d %d\n\r", imu.ax, imu.ay, imu.az);
-        pc.printf("mag: %d %d %d\n\n\r", imu.mx, imu.my, imu.mz);
+        pc.printf("gyro: %d %d %d\n\r", dof.gx, dof.gy, dof.gz);
+        pc.printf("accel: %d %d %d\n\r", dof.ax, dof.ay, dof.az);
+        pc.printf("mag: %d %d %d\n\n\r", dof.mx, dof.my, dof.mz);
         wait(1);
     }
 }
