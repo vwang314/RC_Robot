9d2102384def47199c93806c104bc2a8a17027ae
Vincent Wang <vwang314@gmail.com>
2020-11-29 00:11:02 -0600

    add position tracking

--- main.cpp
+++ main.cpp
@@ -9,31 +9,38 @@ Serial blue(p13, p14);
 Motor left(p21, p25, p26, 1);
 Motor right(p22, p30, p29, 1);
 LSM9DS1 imu(p28, p27, 0xD6, 0x3C);
+bool originSet = false;
+bool returnOrigin = false;
+bool moving = false;
 
 void dist1(int distance){
-    printf("Sens1 Distance %d mm\r\n", distance);
     if(distance < 50){
+        moving = false;
+        printf("Sens1 Distance %d mm\r\n", distance);
         left.stop(0.5);
         right.stop(0.5);
     }
 }
 void dist2(int distance){
-    printf("Sens2 Distance %d mm\r\n", distance);
     if(distance < 50){
+        moving = false;
+        printf("Sens2 Distance %d mm\r\n", distance);
         left.stop(0.5);
         right.stop(0.5);
     }
 }
 void dist3(int distance){
-    printf("Sens3 Distance %d mm\r\n", distance);
     if(distance < 50){
+        moving = false;
+        printf("Sens3 Distance %d mm\r\n", distance);
         left.stop(0.5);
         right.stop(0.5);
     }
 }
 void dist4(int distance){
-    printf("Sens4 Distance %d mm\r\n", distance);
     if(distance < 50){
+        moving = false;
+        printf("Sens4 Distance %d mm\r\n", distance);
         left.stop(0.5);
         right.stop(0.5);
     }
@@ -50,20 +57,46 @@ void check_dist_func(void const *args){
         sens2.checkDistance();
         sens3.checkDistance();
         sens4.checkDistance();
-        Thread::wait(5);
+        Thread::wait(1);
     }
 }
 
 void check_imu_func(void const *args){
+    float x_pos = 0.0;
+    float y_pos = 0.0;
+    float x_vel1 = 0.0;
+    float x_vel2 = 0.0;
+    float y_vel1 = 0.0;
+    float y_vel2 = 0.0;
+    float time = 0.0;
+    float start = 0.0;
+    Timer t;
+    t.start();
     while(1){
-        imu.readTemp();
-        imu.readMag();
-        imu.readGyro();
-
-        pc.printf("gyro: %d %d %d\n\r", imu.gx, imu.gy, imu.gz);
-        pc.printf("accel: %d %d %d\n\r", imu.ax, imu.ay, imu.az);
-        pc.printf("mag: %d %d %d\n\n\r", imu.mx, imu.my, imu.mz);
-        Thread::wait(1000);
+        if(moving){
+            //to reduce noise, position is not constantly updated from IMU; only update position when motor is set to move
+            imu.readAccel();
+            pc.printf("x pos: %9f\n\r", x_pos);
+            pc.printf("y pos: %9f\n\n\r", y_pos);
+            Thread::wait(1);
+            time = t.read() - start;
+            start = t.read();
+            x_vel2 = x_vel1 + imu.calcAccel(imu.ax)*9.80665*time;
+            y_vel2 = y_vel1 - imu.calcAccel(imu.ay)*9.80665*time;
+            x_pos = x_pos + (x_vel1+x_vel2)/2*time;
+            y_pos = y_pos + (y_vel1+y_vel2)/2*time;
+            x_vel1 = x_vel2;
+            y_vel1 = y_vel2;
+        } else {
+            //robot is not moving so reset timer and set velocities to 0
+            t.reset();
+            x_vel1 = 0.0;
+            x_vel2 = 0.0;
+            y_vel1 = 0.0;
+            y_vel2 = 0.0;
+            start = 0.0;
+            time = 0.0;
+        }
     }
 }
 
@@ -74,12 +107,13 @@ int main(){
     }
     imu.calibrate();
     wait(10);
+    pc.printf("\n\rReady\n\n\r");
     sens1.startUpdates();
     sens2.startUpdates();
     sens3.startUpdates();
     sens4.startUpdates();
     Thread Check_Dist(check_dist_func);
-    //Thread Check_IMU(check_imu_func);
+    Thread Check_IMU(check_imu_func);
     char bnum=0;
     char bhit=0;
 
@@ -93,6 +127,7 @@ int main(){
                         case '1': //number button 1
                             if (bhit=='1') {
                                 //add hit code here
+                                originSet = true;
                             } else {
                                 //add release code here
                             }
@@ -100,6 +135,7 @@ int main(){
                         case '2': //number button 2
                             if (bhit=='1') {
                                 //add hit code here
+                                returnOrigin = true;
                             } else {
                                 //add release code here
                             }
@@ -120,18 +156,22 @@ int main(){
                             break;
                         case '5': //button 5 up arrow
                             if (bhit=='1') {
+                                moving = true;
                                 left.speed(0.3);
                                 right.speed(0.3);
                             } else {
+                                moving = false;
                                 left.stop(0.5);
                                 right.stop(0.5);
                             }
                             break;
                         case '6': //button 6 down arrow
                             if (bhit=='1') {
+                                moving = true;
                                 left.speed(-0.3);
                                 right.speed(-0.3);
                             } else {
+                                moving = false;
                                 left.stop(0.5);
                                 right.stop(0.5);
                             }
@@ -160,6 +200,6 @@ int main(){
                 }
             }
         }
-        Thread::wait(5);
+        Thread::wait(1);
     }
 }
